138bc7f5bacadc41171a72320b9a2b113818dae5
suprajasunugu <suprajasunuguri@gmail.com>
2023-11-15 17:44:43 +0530

    checking staged option

---
+++ FoodDelivery.Customer.Api/Controller/ValuesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodDelivery.Customer.Api.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        //added new file for checking the stage option
+    }
+}
