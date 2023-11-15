855c74df1301caaf1bb9d3d769670873564409be
Supraja <Supraja@DESKTOP-HS6R9P4>
2023-11-15 16:49:26 +0530

    Add project files.

---
+++ FoodDelivery.Customer.Api/Controller/LocationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodDelivery.Customer.Api.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        //create a method which is used to search the location based on pin code
+    //    [HttpGet]
+    //    public ActionResult SearchLocation([FromQuery] int pincode)
+    //    {
+    //        return 
+    //    }
+    }
+}
